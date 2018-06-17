8940fee77925ed4282371b8d266a98d0afb77359
XXMrHyde <xxmrhyde@gmx.co.uk>
2018-06-17 22:52:02 +0200

    Theming: Accent color (part 5), (1/4):
    (1/4) : android_external_dkcolorpicker
    (2/4) : android_packages_apps_DKNotificationTest
    (3/4) : android_packages_apps_DKSettings
    (4/4) : android_packages_apps_DKWallpapers
    
    Fix color picker theme when activity was recreated (in general)
    
    When the ColorPickerActivity was recreated by a theme or theme option change,
    the ColorPickerActivity was recreated with the old theme related values
    which caused the old theme and theme options got applied again.
    So don't put theme related values (which may change)
    to the ColorPickerActivity start Intent, and use the ThemeColorHelper and ThemeHelper
    to obtain these values instead.
    
    This will also fix the theme on automatically activity recreations,
    (like orientation changes, ...).

--- src/net/darkkatrom/dkcolorpicker/ColorPickerActivity.java
+++ src/net/darkkatrom/dkcolorpicker/ColorPickerActivity.java
@@ -24,46 +24,51 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.android.internal.util.darkkat.ThemeColorHelper;
+import com.android.internal.util.darkkat.ThemeHelper;
 
 import net.darkkatrom.dkcolorpicker.fragment.ColorPickerFragment;
 import net.darkkatrom.dkcolorpicker.preference.ColorPickerPreference;
 
 public class ColorPickerActivity extends Activity {
-    public static final String KEY_THEME_RES_ID                = "theme_res_id";
-    public static final String KEY_CUSTOMIZE_COLORS            = "customize_colors";
-    public static final String KEY_STATUS_BAR_COLOR            = "status_bar_color";
-    public static final String KEY_PRIMARY_COLOR               = "primary_color";
-    public static final String KEY_NAVIGATION_BAR_COLOR        = "navigation_bar_color";
-    public static final String KEY_COLORIZE_NAVIGATION_BAR     = "colorize_navigation_bar";
-    public static final String KEY_LIGHT_STATUS_BAR            = "light_status_bar";
-    public static final String KEY_LIGHT_ACTION_BAR            = "light_action_bar";
-    public static final String KEY_LIGHT_NAVIGATION_BAR        = "light_navigation_bar";
-    public static final String KEY_IS_WHITEOUT_THEME           = "is_whiteout_theme";
-    public static final String KEY_IS_BLACKOUT_THEME           = "is_blackout_theme";
-    public static final String KEY_THEME_OVERLAY_ACCENT_RES_ID = "theme_res_id";
+    public static final String KEY_SUPPORTS_THEMING         = "supports_theming";
+    public static final String KEY_THEME_RES_ID             = "theme_res_id";
+    public static final String KEY_THEME_LIGHT_AB_RES_ID    = "theme_light_ab_res_id";
+    public static final String KEY_THEME_LIGHT_SB_RES_ID    = "theme_light_sb_res_id";
+    public static final String KEY_THEME_LIGHT_NB_RES_ID    = "theme_light_nb_res_id";
+    public static final String KEY_THEME_LIGHT_AB_NB_RES_ID = "theme_light_ab_nb_res_id";
+    public static final String KEY_THEME_LIGHT_SB_NB_RES_ID = "theme_light_sb_nb_res_id";
 
+    private boolean mSupportsTheming = false;
     private int mThemeResId = 0;
     private boolean mCustomizeColors = false;
+    private int mStatusBarColor = 0;
+    private int mDefaultPrimaryColor = 0;
     private int mPrimaryColor = 0;
+    private int mNavigationColor = 0;
     private boolean mColorizeNavigationBar = false;
     private boolean mLightStatusBar = false;
     private boolean mLightActionBar = false;
     private boolean mLightNavigationBar = false;
+    private boolean mIsBlackoutTheme = false;
+    private boolean mIsWhiteoutTheme = false;
     private int mThemeOverlayAccentResId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            mSupportsTheming = extras.getBoolean(KEY_SUPPORTS_THEMING, false);
+            if (mSupportsTheming) {
+                updateTheme(extras);
+            }
+        }
+
         if (savedInstanceState == null) {
             Fragment f = new ColorPickerFragment();
-            Bundle extras = getIntent().getExtras();
             if (extras != null) {
-                mThemeResId = extras.getInt(KEY_THEME_RES_ID, 0);
                 f.setArguments(extras);
-                if (mThemeResId > 0) {
-                    updateTheme(extras);
-                }
             }
             getFragmentManager().beginTransaction()
                     .replace(android.R.id.content, f)
@@ -77,20 +82,36 @@ public class ColorPickerActivity extends Activity {
     }
 
     public void updateTheme(Bundle extras) {
-        mCustomizeColors = extras.getBoolean(KEY_CUSTOMIZE_COLORS, false);
-        int statusBarColor = extras.getInt(KEY_STATUS_BAR_COLOR, 0);
-        mPrimaryColor = extras.getInt(KEY_PRIMARY_COLOR, 0);
-        int navigationColor = extras.getInt(KEY_NAVIGATION_BAR_COLOR, 0);
-        mColorizeNavigationBar = extras.getBoolean(KEY_COLORIZE_NAVIGATION_BAR, false);
-        mLightStatusBar = extras.getBoolean(KEY_LIGHT_STATUS_BAR, false);
-        mLightActionBar = extras.getBoolean(KEY_LIGHT_ACTION_BAR, false);
-        mLightNavigationBar = extras.getBoolean(KEY_LIGHT_NAVIGATION_BAR, false);
-        boolean isWhiteoutTheme = extras.getBoolean(KEY_IS_WHITEOUT_THEME, false);
-        boolean isBlackoutTheme = extras.getBoolean(KEY_IS_BLACKOUT_THEME, false);
+        mCustomizeColors = ThemeColorHelper.customizeColors(this);
+        mDefaultPrimaryColor = 0xff2196f3;
+        mStatusBarColor = ThemeColorHelper.getStatusBarBackgroundColor(this, mDefaultPrimaryColor);
+        mPrimaryColor = ThemeColorHelper.getPrimaryColor(this, mDefaultPrimaryColor);
+        mNavigationColor = ThemeColorHelper.getNavigationBarBackgroundColor(this, mDefaultPrimaryColor);
+        mColorizeNavigationBar = ThemeColorHelper.colorizeNavigationBar(this);
+        mLightStatusBar = ThemeColorHelper.lightStatusBar(this, mDefaultPrimaryColor);
+        mLightActionBar = ThemeColorHelper.lightActionBar(this, mDefaultPrimaryColor);
+        mLightNavigationBar = ThemeColorHelper.lightNavigationBar(this, mDefaultPrimaryColor);
+        mIsBlackoutTheme = ThemeHelper.isBlackoutTheme(this);
+        mIsWhiteoutTheme = ThemeHelper.isWhiteoutTheme(this);
 
-        setTheme(mThemeResId);
+        if (mLightActionBar && mLightNavigationBar) {
+            mThemeResId = mLightStatusBar
+                    ? extras.getInt(KEY_THEME_LIGHT_SB_NB_RES_ID, 0)
+                    : extras.getInt(KEY_THEME_LIGHT_AB_NB_RES_ID, 0);
+        } else if (mLightActionBar) {
+            mThemeResId = mLightStatusBar
+                    ? extras.getInt(KEY_THEME_LIGHT_SB_RES_ID, 0)
+                    : extras.getInt(KEY_THEME_LIGHT_AB_RES_ID, 0);
+        } else if (mLightNavigationBar) {
+            mThemeResId = extras.getInt(KEY_THEME_LIGHT_NB_RES_ID, 0);
+        } else {
+            mThemeResId = extras.getInt(KEY_THEME_RES_ID, 0);
+        }
+        if (mThemeResId > 0) {
+            setTheme(mThemeResId);
+        }
 
-        mThemeOverlayAccentResId = extras.getInt(KEY_THEME_OVERLAY_ACCENT_RES_ID, 0);
+        mThemeOverlayAccentResId = ThemeColorHelper.getThemeOverlayAccentResId(this);
         if (mThemeOverlayAccentResId > 0) {
             getTheme().applyStyle(mThemeOverlayAccentResId, true);
         }
@@ -119,19 +140,19 @@ public class ColorPickerActivity extends Activity {
             getWindow().getDecorView().setSystemUiVisibility(newFlags);
         }
 
-        if (mCustomizeColors && !isBlackoutTheme && !isWhiteoutTheme) {
-            getWindow().setStatusBarColor(statusBarColor);
+        if (mCustomizeColors && !mIsBlackoutTheme && !mIsWhiteoutTheme) {
+            getWindow().setStatusBarColor(mStatusBarColor);
             getActionBar().setBackgroundDrawable(new ColorDrawable(mPrimaryColor));
         }
-        if (navigationColor != 0) {
-            getWindow().setNavigationBarColor(navigationColor);
+        if (mNavigationColor != 0) {
+            getWindow().setNavigationBarColor(mNavigationColor);
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (mThemeResId > 0) {
+        if (mSupportsTheming) {
             boolean customizeColors = ThemeColorHelper.customizeColors(this);
             int primaryColor = ThemeColorHelper.getPrimaryColor(this, 0xff2196f3);
             boolean colorizeNavigationBar = ThemeColorHelper.colorizeNavigationBar(this);
